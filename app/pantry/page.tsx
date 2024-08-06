4735d568cf07ae9694f362056bd539d743c9d08e
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2024-08-06 10:10:24 +0100

    feat: pantry page route

---
+++ app/pantry/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import PantryTracker from "@/components/pantry/pantry";
+
+type Props = {};
+
+const PantryMain = (props: Props) => {
+  return <PantryTracker />;
+};
+
+export default PantryMain;
